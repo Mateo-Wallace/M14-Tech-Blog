ae0faf739c8b835fdc414f639d29077d2ea23c76
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-23 00:52:39 -0500

    delete button functional

---
+++ public/js/delete.js
@@ -0,0 +1,19 @@
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/post/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+
+  document
+  .querySelector('.delete')
+  .addEventListener('click', delButtonHandler);
