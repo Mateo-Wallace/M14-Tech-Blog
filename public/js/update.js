559c12f67ed47021e6a49f3d89d457ae309c708e
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-23 01:20:22 -0500

    update js updated

--- public/js/update.js
+++ public/js/update.js
@@ -1,53 +1,24 @@
-const editPlaylist = async (id, newTitle) => {
-    await fetch(`/api/playlists/edit/${id}`, {
-      method: "PUT",
-      body: JSON.stringify({
-        newTitle,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then(document.location.reload())
-      .catch((err) => console.log(err));
-  };
-  
-  const handlePlaylistEdit = async (e) => {
-    // Prevents the click listener for the list from being called when the button inside of it is clicked
-    e.stopPropagation();
-  
-    const playlist = e.target;
-    const playlistName = document.querySelector(".titleLink");
-    playlistName.href = "#";
-    const playlistId = playlist.parentElement.getAttribute("data-id");
-    const originalTitleEl = document.getElementById(playlistId);
-    const newInput = document.createElement(`input`);
-    const originalTitle = document.getElementById(playlistId).textContent;
-    const saveBtn = playlist.parentElement.nextElementSibling;
-  
-    saveBtn.classList.remove("hide");
-    newInput.setAttribute("id", "newTitle");
-    newInput.value = originalTitle;
-    originalTitleEl.parentElement.insertBefore(newInput, originalTitleEl);
-    originalTitleEl.remove();
-  };
-  
-  const handlePlaylistSave = async (e) => {
-    e.stopPropagation();
-    const playlist = e.target;
-    const playlistId = playlist.parentElement.getAttribute("data-id");
-    const newTitle = document.getElementById("newTitle").value;
-  
-    console.log(newTitle);
-  
-    const response = await editPlaylist(playlistId, newTitle);
-  
-    return response;
-  };
-  
-  const editBtn = document.querySelectorAll(".update");
-  editBtn.forEach((btn) => btn.addEventListener("click", handlePlaylistEdit));
-  
-  const saveButton = document.querySelectorAll(".save-btn");
-  saveButton.forEach((btn) => btn.addEventListener("click", handlePlaylistSave));
+const postFormHandler = async function (event) {
+    event.preventDefault();
+    const post_idString = document.querySelector('#post-id').innerHTML;
+    const id = +post_idString
+    const title = document.querySelector('#post-title').value;
+    const content = document.querySelector('#post-body').value;
+
+    if (content && title) {
+        await fetch(`/api/post${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                title,
+                content
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        document.location.reload();
+    }
+};
+
+document.querySelector('#new-post-form').addEventListener("click", postFormHandler);
